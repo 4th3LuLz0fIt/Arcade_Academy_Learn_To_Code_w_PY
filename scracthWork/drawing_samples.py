494465e2212ae3607be45e018ad8ed0e551ce386
AakeemCodes <19845846+4th3LuLz0fIt@users.noreply.github.com>
2022-02-01 21:18:42 -0500

    Initial
    Lab_1
    
    How to draw with your computer

---
+++ scracthWork/drawing_samples.py
@@ -0,0 +1,8 @@
+"""Code to show how to draw uing Python and Arcade"""
+
+#import the arcade library
+import arcade
+
+# Create a window for drawing
+
+arcade.open_window(600, 600, 'Drawing Example')
